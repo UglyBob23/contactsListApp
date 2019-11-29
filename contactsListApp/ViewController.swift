a7f7247fc56ab711010a3d0c79b113ba963d50e8
Vladimir <UglyBob23@yandex.ru>
2019-11-29 22:28:41 +0500

    Initial Commit

---
+++ contactsListApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  contactsListApp
+//
+//  Created by Владимир Паутов on 29.11.2019.
+//  Copyright © 2019 Владимир Паутов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
